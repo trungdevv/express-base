3e3b74a40baaeaac9375329b4c75824f9078c18e
trung.tran <trantrungse1209@gmail.com>
2024-01-08 17:02:40 +0700

    update jwt

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 import Joi from "joi";
 import { userRepository } from "../repositories/index.js";
-
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
 // Login
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
@@ -8,20 +9,38 @@ const loginSchema = Joi.object({
 });
 
 const login = async (req, res) => {
-  const { value, error } = loginSchema.validate;
+  const { value, error } = loginSchema.validate(req.body);
   if (error) {
     return res.send(error.status);
   }
   const { email, password } = req.body;
+  try {
+   let user =  await userRepository.login({ email, password });
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // Register
-
+const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(3).max(16).required(),
+});
 const register = async (req, res) => {
   const { email, password, phoneNumber, address } = req.body;
-  await userRepository.register({ email, password, phoneNumber, address });
-  res.status(200).json({ message: "" });
-  let newUser = await userRepository.login({ email, password });
-  res.status(200).json({ message: "Register OK" });
+  try {
+    const user = await userRepository.register({
+      email,
+      password,
+      phoneNumber,
+      address,
+    });
+    res.status(200).json({ message: "Register OK", data: user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+  // let newUser = await userRepository.login({ email, password });
 };
 export default { login, register: register };
--- repositories/user.js
+++ repositories/user.js
@@ -1,7 +1,19 @@
 import { User } from "../models/index.js";
 import bcrypt from "bcrypt";
-const login = async (req, res) => {
-  console.log("ok hahs");
+const login = async ({ email, password }) => {
+  let existingUser = await User.findOne({ email: email }).exec();
+  console.log("🚀 ~ file: user.js:5 ~ login ~ existingUser:", existingUser);
+  if (existingUser) {
+    let isMatch = await bcrypt.compare(password, existingUser.password);
+
+    if (!!isMatch) {
+      //create java token
+    } else {
+      throw new Error("Wrong email and password");
+    }
+  } else {
+    throw new Error("Wrong email and password");
+  }
 };
 const register = async ({ email, password, phoneNumber, address }) => {
   try {
@@ -13,14 +25,14 @@ const register = async ({ email, password, phoneNumber, address }) => {
       password,
       parseInt(process.env.SALT_ROUNDS)
     );
-    // console.log("🚀 ~ file: user.js:13 ~ register ~ hashPassword:", hashPassword)
+   
     const newUser = await User.create({ email, password: hashPassword });
     return {
       ...newUser._doc,
-      password: "Not show"
+      password: "Not show",
     };
   } catch (error) {
-    console.log(error.message);
+    throw new Error(error.message);
   }
 };
 export default { login, register };
